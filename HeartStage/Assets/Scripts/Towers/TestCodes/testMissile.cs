ea7e81281320955deda5140ba6a0bc6ac7816d2f
윤준모 <ojm01088021296@gmail.com>
2025-11-06 21:46:59 +0900

    타워 공격 간단 구현

---
+++ HeartStage/Assets/Scripts/Towers/TestCodes/testMissile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class testMissile : MonoBehaviour
+{
+	private float moveSpeed;
+	private Vector3 dir;
+	private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+		if (collision.CompareTag(Tag.Enemy))
+		{
+			Destroy(collision.gameObject);
+			Destroy(gameObject);
+		}
+    }
+
+    private void Update()
+    {
+		transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+
+    public void SetMissile(float speed, Vector3 dir, float damage)
+	{
+		moveSpeed = speed;
+		this.dir = dir;
+		this.damage = damage;
+	}
+}
