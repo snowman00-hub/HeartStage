a7d109b48794e635563ba4709dcadd3f3d9cd542
윤준모 <ojm01088021296@gmail.com>
2025-11-10 16:35:54 +0900

    대기

---
+++ HeartStage/Assets/Scripts/CameraAutoScaler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+[RequireComponent(typeof(Camera))]
+public class CameraAutoScaler : MonoBehaviour
+{
+    [Header("기준 비율 (예: 9:16 = 0.5625)")]
+    public float referenceAspect = 9f / 16f;
+    [Header("기준 orthographicSize")]
+    public float referenceOrthoSize = 9.6f;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        UpdateCameraSize();
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        UpdateCameraSize(); // 에디터에서 실시간 반영
+#endif
+    }
+
+    private void UpdateCameraSize()
+    {
+        float currentAspect = (float)Screen.width / Screen.height;
+
+        // 기준보다 가로가 넓은 경우 → 더 많이 보이게
+        if (currentAspect > referenceAspect)
+        {
+            cam.orthographicSize = referenceOrthoSize;
+        }
+        else // 기준보다 세로가 긴 경우 → 더 넓게 보이게
+        {
+            float scale = referenceAspect / currentAspect;
+            cam.orthographicSize = referenceOrthoSize * scale;
+        }
+    }
+}
