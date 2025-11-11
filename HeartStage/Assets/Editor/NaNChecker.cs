e15a582c6500071d6ceab5e7407a1b12e01a5f6e
윤준모 <ojm01088021296@gmail.com>
2025-11-11 09:22:59 +0900

    nan 오류 해결

---
+++ HeartStage/Assets/Editor/NaNChecker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class NaNChecker : EditorWindow
+{
+    [MenuItem("Tools/Check for NaN Transforms")]
+    static void CheckForNaN()
+    {
+        foreach (var t in FindObjectsOfType<Transform>())
+        {
+            Vector3 pos = t.position;
+            if (float.IsNaN(pos.x) || float.IsNaN(pos.y) || float.IsNaN(pos.z) ||
+                float.IsInfinity(pos.x) || float.IsInfinity(pos.y) || float.IsInfinity(pos.z))
+            {
+                Debug.LogWarning($"⚠️ {t.name} has invalid position: {pos}", t.gameObject);
+            }
+        }
+    }
+}
