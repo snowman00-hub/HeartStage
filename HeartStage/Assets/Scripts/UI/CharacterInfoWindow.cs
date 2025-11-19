4f838913ee3117ba6dfdae6560d078e0a3cad7ed
윤준모 <ojm01088021296@gmail.com>
2025-11-19 15:58:12 +0900

    캐릭터 정보창 UI 추가중

---
+++ HeartStage/Assets/Scripts/UI/CharacterInfoWindow.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterInfoWindow : MonoBehaviour
+{
+    public Image characterImage;
+    public TextMeshProUGUI characterName;
+    public Image attributeIcon;
+    public TextMeshProUGUI rankText; // 나중에 별로 변경
+    public TextMeshProUGUI descText;                                             
+
+    public void Init(CharacterData data)
+    {
+
+    }
+}
