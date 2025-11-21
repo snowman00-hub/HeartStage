8164f019b8733bf42981d0a579270a511c885f2a
윤준모 <ojm01088021296@gmail.com>
2025-11-21 17:24:27 +0900

    로비 UI 디자인 적용중

---
+++ HeartStage/Assets/Scripts/UI/SafeAreaFitter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform panel;
+    private Rect lastSafeArea = Rect.zero;
+
+    void Awake()
+    {
+        panel = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void Update()
+    {
+        if (Screen.safeArea != lastSafeArea)
+            ApplySafeArea();
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+        lastSafeArea = safeArea;
+
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        panel.anchorMin = anchorMin;
+        panel.anchorMax = anchorMax;
+    }
+}
