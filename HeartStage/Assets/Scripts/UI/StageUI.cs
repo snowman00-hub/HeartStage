4921a1e964773d7f96577394180dcc65c84288fd
윤준모 <ojm01088021296@gmail.com>
2025-11-21 15:03:15 +0900

    수정중

--- HeartStage/Assets/Scripts/UI/StageUI.cs
+++ HeartStage/Assets/Scripts/UI/StageUI.cs
@@ -43,26 +43,26 @@ public class StageUI : MonoBehaviour
     {
         if (stageNumber == 0)
         {
-            waveCountText.text = $"Tutorial {waveOrder}";
+            waveCountText.text = $"Tutorial\nWave {waveOrder}";
         }
         else
         {
             var currentStage = StageManager.Instance.GetCurrentStageData();
             if (currentStage != null)
             {
-                waveCountText.text = $"{currentStage.stage_step1}-{currentStage.stage_step2}스테이지\n{waveOrder}웨이브";
+                waveCountText.text = $"{currentStage.stage_step1}-{currentStage.stage_step2}스테이지\nWave {waveOrder}";
             }
             else
             {
                 // currentStage가 null일 경우 stageNumber를 직접 사용
-                waveCountText.text = $"{stageNumber}-1스테이지\n{waveOrder}웨이브";
+                waveCountText.text = $"{stageNumber}-1스테이지\nWave {waveOrder}";
             }
         }
     }
 
     public void SetReaminMonsterCount(int remainMonsterCount)
     {
-        remainMonsterCountText.text = $"남은 적군\n{remainMonsterCount}";
+        remainMonsterCountText.text = $"{remainMonsterCount}";
     }
 
     private void OnBossSkillButton1Clicked()
