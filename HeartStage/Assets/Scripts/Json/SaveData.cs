f955f45daad67eb9640ba595bb23b6fca83682fa
김의중 <htp31698@naver.com>
2025-11-24 17:35:28 +0900

    SaveData 추가 / 초기 캐릭터 추가

--- HeartStage/Assets/Scripts/Json/SaveData.cs
+++ HeartStage/Assets/Scripts/Json/SaveData.cs
@@ -14,6 +14,9 @@ public class SaveDataV1 : SaveData
 {
     public Dictionary<int, int> itemList = new Dictionary<int, int>(); // 아이템 ID와 수량을 저장
     public List<int> clearWaveList = new List<int>(); // 클리어한 웨이브 ID를 저장 -> 최초 보상 체크
+    public List<int> ownedIds = new List<int>(); // 보유 아이디 집합 (중복 방지)
+    public Dictionary<string, bool> unlockedByName = new Dictionary<string, bool>(); // 해금 여부를 Name별로 저장
+    public Dictionary<int, int> expById = new Dictionary<int, int>(); // 경험치를 ID별로 저장
 
     public SaveDataV1()
     {
