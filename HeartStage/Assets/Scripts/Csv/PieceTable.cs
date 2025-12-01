34d328311887f206c87485a3b896f9cdeff0dd1b
윤준모 <ojm01088021296@gmail.com>
2025-12-01 13:21:07 +0900

    수정

---
+++ HeartStage/Assets/Scripts/Csv/PieceTable.cs
@@ -0,0 +1,55 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class PieceData
+{
+    public int piece_ingrd { get; set; }
+    public int piece_result { get; set; }
+    public int piece_ingrd_amount { get; set; }
+    public string info { get; set; }
+}
+
+public class PieceTable : DataTable
+{
+    public static readonly string Unknown = "레벨업 ID 없음";
+
+    private readonly Dictionary<int, PieceData> table = new Dictionary<int, PieceData>();
+
+    public override async UniTask LoadAsync(string filename)
+    {
+        table.Clear();
+        AsyncOperationHandle<TextAsset> handle = Addressables.LoadAssetAsync<TextAsset>(filename);
+        TextAsset ta = await handle.Task;
+
+        if (!ta)
+        {
+            Debug.LogError($"TextAsset 로드 실패: {filename}");
+            return;
+        }
+
+        var list = LoadCSV<PieceData>(ta.text);
+
+        foreach (var item in list)
+        {
+            if (!table.ContainsKey(item.piece_ingrd))
+            {
+                table.Add(item.piece_ingrd, item);
+            }
+        }
+
+        Addressables.Release(handle);
+    }
+
+    public PieceData Get(int levelUpId)
+    {
+        if (!table.ContainsKey(levelUpId))
+        {
+            return null;
+        }
+
+        return table[levelUpId];
+    }
+}
