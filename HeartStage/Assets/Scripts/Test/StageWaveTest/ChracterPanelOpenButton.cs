828c1f7e53ecc983c77091eff950eebfa42ad58c
김의중 <htp31698@naver.com>
2025-12-01 11:36:50 +0900

    TestScene 기능 추가 완료

---
+++ HeartStage/Assets/Scripts/Test/StageWaveTest/ChracterPanelOpenButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChracterPanelOpenButton : MonoBehaviour
+{
+    public CharacterTestPanel characterPanel;
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(OnClick_OpenCharacter);
+    }
+
+    private void OnDisable()
+    {
+        button.onClick.RemoveListener(OnClick_OpenCharacter);
+    }
+    void OnClick_OpenCharacter()
+    {
+        characterPanel.Open();
+    }
+}
